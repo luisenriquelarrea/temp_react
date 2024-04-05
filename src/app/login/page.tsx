70f61e8b3141415bcdaec225015bd0d285447812
artplusplusmx <enrique.larrea@alumno.udg.mx>
2024-04-05 12:57:31 -0600

    RFX

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,54 @@
-import LoginForm from '@/app/ui/login-form';
+"use client";
+
+import { useState } from 'react';
 
 const LoginPage = () => {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event: any) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+      }
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        console.log(inputs);
+    }
+
     return(
         <div className="container is-fluid">
-            <LoginForm />
+            <form onSubmit={handleSubmit}>
+                <div className="field">
+                    <label className="label">Username</label>
+                    <div className="control">
+                        <input 
+                            className="input" 
+                            type="text" 
+                            name="name" 
+                            value={ inputs.name || "" }
+                            onChange={ handleChange }
+                            placeholder="username" />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Password</label>
+                    <div className="control">
+                        <input 
+                            className="input" 
+                            type="password" 
+                            name="password"
+                            value={ inputs.password || "" } 
+                            onChange={ handleChange }
+                            placeholder="Password" />
+                    </div>
+                </div>
+                <div className="field">
+                    <div className="control">
+                        <button type="submit" className="button is-success">Login</button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
