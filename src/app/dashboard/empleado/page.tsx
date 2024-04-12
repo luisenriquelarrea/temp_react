5b3ed6e65906a63a9f80302d1c16e94c1f54ed8e
artplusplusmx <enrique.larrea@alumno.udg.mx>
2024-04-12 12:20:31 -0600

    RFX

--- src/app/dashboard/empleado/page.tsx
+++ src/app/dashboard/empleado/page.tsx
@@ -1,12 +1,34 @@
-import Breadcrumb from '@/app/ui/dashboard/BreadCrumb';
+"use client"
+
+import { useState } from "react";
 import Alta from '@/app/ui/dashboard/Alta';
 import Table from '@/app/ui/dashboard/Table';
 const Page = () => {
+    const [showAlta, setShowAlta] = useState(false);
+    const [showLista, setShowLista] = useState(false);
+
+    const navigateBreadcrumb = (breadcrumb: String) => {
+        if(breadcrumb === 'alta')
+            setShowAlta( !showAlta )
+        else
+            setShowLista( !showLista )
+    }
+
     return(
         <>
-            <Breadcrumb />
-            <Alta />
-            <Table />
+            <nav className="breadcrumb no-print" aria-label="breadcrumbs">
+                <ul>
+                    <li className="my-bread"><b> Empleados </b></li>
+                    <li>
+                        <a onClick={() => navigateBreadcrumb("alta")}>Alta</a>
+                    </li>
+                    <li>
+                        <a onClick={() => navigateBreadcrumb("lista")}>Lista</a>
+                    </li>
+                </ul>
+            </nav>
+            { showAlta ? <Alta /> : null }
+            { showLista ? <Table /> : null }
         </>
     );
 }
