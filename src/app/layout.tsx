59d95bedde014483431ce76865c14a99151e65ef
Luis Enrique Larrea <enrique.larrea@alumno.udg.mx>
2024-05-27 23:07:53 -0600

    RFX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,16 @@ export default function RootLayout({
   //const { user, login, logout, setUser } = useAuth();
   return (
     <html lang="en">
-      
+        <head>
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        </head>
         <body 
           className={inter.className} 
           suppressHydrationWarning={true}>
             {children}
         </body>
-      
     </html>
   );
 }
