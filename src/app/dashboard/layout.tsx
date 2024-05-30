2ca2112d067fae6529cef9c17997601dc59de94c
Luis Enrique Larrea <enrique.larrea@alumno.udg.mx>
2024-05-29 21:25:30 -0600

    RFX

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,13 @@
 import SideNav from '@/app/ui/dashboard/SideNav';
+import Header from '@/app/ui/dashboard/Header';
  
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="w3-bar w3-top w3-blue w3-large my-top-bar">
-          <button className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey"><i className="fa fa-bars"></i> Menu</button>
-          <span className="w3-bar-item w3-right">Visual Studio Code</span>
-      </div>
-      <div className="my-sidebar">
-          <SideNav />
-      </div>
-      <div>
-        <div className="w3-main my-main" >
+      <Header />
+      <SideNav />
+      <div className="w3-main my-main" >
           {children}
-        </div>
       </div>
     </>
   );
--- src/app/ui/dashboard/SideNav.tsx
+++ src/app/ui/dashboard/SideNav.tsx
@@ -3,16 +3,6 @@ import NavLinks from '@/app/ui/dashboard/NavLinks';
 const SideNav = () => {
     return (
         <nav className="w3-sidebar w3-collapse w3-white my-sidebar">
-            <br/>
-            <div className="w3-container w3-row">
-                <div className="w3-col s4">
-                
-                </div>
-                <div className="w3-col s8 w3-bar">
-
-                </div>
-            </div>
-            <hr/>
             <div className="w3-bar-block">
                 <NavLinks />
             </div>
