060bd87edb472b8505e4487ec2f716e0e78676f2
luisenriquelarrea <enrique.larrea@alumno.udg.mx>
2024-07-04 09:53:21 -0600

    RFX

---
+++ src/app/dashboard/accion_basica/page.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import { memo } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import Alta from '@/app/ui/dashboard/Alta';
+import Table from '@/app/ui/dashboard/Table';
+import { arrayColumn } from '../../funciones';
+import { User, Accion } from '../../entities';
+import {getSeccionMenu, getBreadcrumbs } from '../../api';
+
+const Page = () => {
+    const { getItem } = useLocalStorage();
+    const [seccionMenuId, setSecccionMenuId] = useState(0);
+    const [seccionMenu, setSecccionMenu] = useState("");
+    const [navbarLabel, setNavbarLabel] = useState("");
+    const [alta, setAlta] = useState(false);
+    const [lista, setLista] = useState(false);
+    const [breadcrumbs, setBreadcrumbs] = useState([]);
+    //const breadcrumbStates = useRef<any>({});
+
+    useEffect(() => {
+        const user: User = JSON.parse(String(getItem("user")));
+        let pathname = window.location.pathname;
+        let seccionMenu = String(pathname).substring(pathname.lastIndexOf("/") + 1);
+        setSecccionMenu(seccionMenu);
+        getSeccionMenu(seccionMenu).then(response => {
+            if(!response.ok){
+                console.log("Error al obtener seccionMenu");
+                console.log(response);
+                return;
+            }
+            response.json().then(data => {
+                setNavbarLabel(data.navbarLabel);
+                const seccionMenuId = data.id;
+                setSecccionMenuId(seccionMenuId);
+                getBreadcrumbs(seccionMenuId, user.grupo).then(response => {
+                    if(!response.ok){
+                        console.log("Error al obtener breadcrumbs");
+                        console.log(response);
+                        return;
+                    }
+                    response.json().then(data => {
+                        let tmp: any = {};
+                        arrayColumn(data, 'callMethod').forEach(key => {
+                            tmp[key] = false;
+                        });
+                        //breadcrumbStates.current = tmp;
+                        if(arrayColumn(data, 'descripcion').includes('read'))
+                            read();
+                        setBreadcrumbs(data);
+                    })
+                }).catch(error => console.error(error));
+            })
+        }).catch(error => console.error(error));
+    }, []);
+
+    const create = () => {
+        if(Boolean(alta) === true)
+            return;
+        setLista(false);
+        setAlta(!alta);
+    }
+
+    const read = () => {
+        if(Boolean(lista) === true)
+            return;
+        setAlta(false);
+        setLista(!lista);
+    }
+
+    /*const navigateBreadcrumb = (evt: any, breadcrumb: string) => {
+        evt.preventDefault();
+        const newStates = breadcrumbStates;
+        for(const prop in newStates)
+            newStates[prop] = false;
+        for(const prop in newStates)
+            (prop === breadcrumb) ? newStates[prop] = true : false;
+        setBreadcrumbStates(newStates);
+        console.log(breadcrumbStates);*/
+        /*for(const prop in breadcrumbStates.current)
+            breadcrumbStates.current[prop] = false;
+        for(const prop in breadcrumbStates.current)
+            (prop === breadcrumb) ? breadcrumbStates.current[prop] = true : false;
+        console.log(breadcrumbStates.current);
+    }*/
+
+    return(
+        <>
+            <nav className="breadcrumb no-print" aria-label="breadcrumbs">
+                <ul>
+                    <li><a className="my-bread" style={{color: 'black'}}><b>{ navbarLabel }</b></a></li>
+                    {breadcrumbs.map((breadcrumb: Accion) => {
+                        return(
+                            <li key={ breadcrumb.descripcion }>
+                                <a 
+                                    onClick={() => eval(String(breadcrumb.callMethod))() }
+                                    key={ breadcrumb.descripcion } >
+                                    <i className={`fa fa-${breadcrumb.icon} fa-fw`}></i>
+                                    {breadcrumb.label}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+            { Boolean(alta) ? <Alta 
+                seccionMenuId={ seccionMenuId } 
+                seccionMenu={ seccionMenu } /> : null }
+            { Boolean(lista) ? <Table
+                seccionMenuId={ seccionMenuId } 
+                seccionMenu={ seccionMenu } /> : null }
+        </>
+    );
+}
+
+export default memo(Page);
