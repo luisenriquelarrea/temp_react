2ba2046bbf57885c4cb1a04b1eb2a10924810c60
luisenriquelarrea <enrique.larrea@alumno.udg.mx>
2024-07-04 10:12:44 -0600

    RFX

--- src/app/ui/dashboard/SideNav.tsx
+++ src/app/ui/dashboard/SideNav.tsx
@@ -1,13 +1,21 @@
+import { useState, useEffect } from "react";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import DropDown from '@/app/ui/dashboard/DropDown';
 
 const SideNav = () => {
     const { removeItem } = useLocalStorage();
+    const { getItem } = useLocalStorage();
+    const [username, setUsername] = useState("");
 
     const logOut = () => {
         removeItem("user");
         window.location.href = '/';
     }
+
+    useEffect(() => {
+        const user = JSON.parse(String(getItem("user")));
+        setUsername(user.username);
+    }, []);
 
     return (
         <nav className="w3-sidebar w3-collapse w3-white my-sidebar">
@@ -16,7 +24,7 @@ const SideNav = () => {
                     <i className="fa fa-users w3-xxxlarge"></i>
                 </div>
                 <div className="w3-col s8 w3-bar">
-                    <span>Welcome, <strong>admin</strong></span><br/>
+                    <span>Welcome, <strong>{ username }</strong></span><br/>
                     <a href="#" className="w3-bar-item w3-button"><i className="fa fa-envelope"></i></a>
                     <a href="#" className="w3-bar-item w3-button"><i className="fa fa-user"></i></a>
                     <a href="#" onClick={logOut} className="w3-bar-item w3-button"><i className="fa fa-power-off"></i></a>
