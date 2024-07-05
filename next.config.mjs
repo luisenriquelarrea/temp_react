48792357609059946a6d3a94ef124a40f038fb3b
luisenriquelarrea <enrique.larrea@alumno.udg.mx>
2024-07-05 14:14:59 -0600

    RFX

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+var isProd = false;
+
 const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    distDir: "build", // Where to export all pages
+    trailingSlash: true,
+    assetPrefix: isProd ? 'http://localhost' : undefined,
+
+
+    // time in seconds of no pages generating during static
+    // generation before timing out
+    staticPageGenerationTimeout: 1000,
     reactStrictMode: false
 };
 
