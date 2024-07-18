500c6f0e171e99a1d7f2e0b1f3ec8d654398c5ba
luisenriquelarrea <enrique.larrea@alumno.udg.mx>
2024-07-17 22:07:03 -0600

    RFX

--- src/app/ui/dashboard/Filters.tsx
+++ src/app/ui/dashboard/Filters.tsx
@@ -2,27 +2,16 @@ import { useState, useEffect } from "react";
 import InputTextFilter  from './InputTextFilter';
 import InputSelectFilter  from './InputSelectFilter';
 import InputDatesFilter  from './InputDatesFilter';
-import { getInputs, getSeccionMenuListFiltered } from '../../api';
+import { getSeccionMenuListFiltered } from '../../api';
 import { SeccionMenuInput } from '../../entities';
 import { objectClean } from '../../funciones';
 
 const Filters = (props: any) => {
-    const [inputs, setInputs] = useState([]);
     const [formData, setFormData] = useState({});
-    const [inputsText, setInputsText] = useState<any>([]);
+    const inputs = props.inputsFilters;
+    const inputsText = ['text', 'password', 'date'];
 
     useEffect(() => {
-        setInputsText(['text', 'password', 'date']);
-        getInputs(props.seccionMenuId, 'filtro').then(response => {
-            if(!response.ok){
-                console.log("Error al obtener inputs");
-                console.log(response);
-                return;
-            }
-            response.json().then(data => {
-                setInputs(data);
-            })
-        }).catch(error => console.error(error));
         window.addEventListener('keypress', e => {
             if(e.key === 'Enter')
                 document.getElementById('btn')?.click();   
@@ -53,7 +42,6 @@ const Filters = (props: any) => {
     }
 
     const handleSubmit = () => {
-        console.log(objectClean(formData));
         getSeccionMenuListFiltered(props.seccionMenu, objectClean(formData)).then(response => {
             if(!response.ok){
                 console.log("Error al obtener lista filtrada");
@@ -61,7 +49,7 @@ const Filters = (props: any) => {
                 return;
             }
             response.json().then(data => {
-                props.setTable(data);
+                props.setDataTable(data);
             })
         }).catch(error => console.error(error));
     }
@@ -76,7 +64,7 @@ const Filters = (props: any) => {
                 })}
                 <div className="column is-12" style={{"padding": 0}}></div>
                 <div className="column is-2">
-                    <button id="btn" onClick={ handleSubmit } className="button is-info is-small is-fullwidth">Filtrar</button>
+                    <button id="btn" onClick={ handleSubmit } className="button is-small is-fullwidth">Filtrar</button>
                 </div>
             </div>
         </>
