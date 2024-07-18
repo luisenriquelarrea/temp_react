c793a94617a80e7e438262c326b6f28e58e6920b
luisenriquelarrea <enrique.larrea@alumno.udg.mx>
2024-07-18 10:12:13 -0600

    RFX

--- src/app/ui/dashboard/Lista.tsx
+++ src/app/ui/dashboard/Lista.tsx
@@ -3,36 +3,36 @@ import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { 
     getInputs, 
     getSeccionMenuList, 
-    getTableActions, 
-    getById, 
-    updateRecord,
-    deleteRecord 
+    getTableActions
 } from '../../api';
-import { User, Accion } from '../../entities';
-import { flipStatus, arrayColumn } from '../../funciones';
-import ModalUpdate from '@/app/ui/dashboard/ModalUpdate';
+import { User } from '../../entities';
+import { arrayColumn } from '../../funciones';
 import Filters from '@/app/ui/dashboard/Filters';
 import Pagination from "./Pagination";
-import { useDownloadExcel } from "react-export-table-to-excel";
+import Table from "./Table";
 
 const Lista = (props: any) => {
     const { getItem } = useLocalStorage();
+    const [inputsFilters, setInputsFilters] = useState([]);
     const [columns, setColumns] = useState([]);
-    const [data, setData] = useState([]);
+    const [dataTable, setDataTable] = useState([]);
     const [tableActions, setTableActions] = useState([]);
     const [xls, setXls] = useState(false);
-    const [showModal, setShowModal] = useState(false);
-    const [recordId, setRecordId] = useState(0);
-    const tableRef = useRef(null);
     const [currentPage, setCurrentPage] = useState(1);
     const [rowsPerPage, setRowsPerPage] = useState(15);
     const [totalRows, setTotalRows] = useState(0);
 
-    const recordInactive = {
-        backgroundColor: "#ffe4f3"
-    };
-
     useEffect(() => {
+        getInputs(props.seccionMenuId, 'filtro').then(response => {
+            if(!response.ok){
+                console.log("Error al obtener inputs filtro");
+                console.log(response);
+                return;
+            }
+            response.json().then(data => {
+                setInputsFilters(data);
+            })
+        }).catch(error => console.error(error));
         getInputs(props.seccionMenuId, 'lista').then(response => {
             if(!response.ok){
                 console.log("Error al obtener inputs");
@@ -70,7 +70,7 @@ const Lista = (props: any) => {
             }
             response.json().then(data => {
                 //setData(setTableToPaginate(data));
-                setData(data);
+                setDataTable(data);
                 setTotalRows(Math.ceil(data.length / rowsPerPage));
             })
         }).catch(error => console.error(error));
@@ -82,161 +82,21 @@ const Lista = (props: any) => {
         return tableData.slice(startIndex, endIndex); 
     }
 
-    const renderAction = (action: Accion, record: any) => {
-        if(action.callMethod === "xls")
-            return null;
-        if(action.callMethod !== "changeStatus")
-            return <i 
-                key={ action.id } 
-                title={ action.label }
-                className={`fa fa-${action.icon} fa-fw`}
-                style={{"fontSize": '14px'}}
-                onClick={() => handleAction(String(action.callMethod), record) } >
-
-                </i>
-        if(action.descripcion === "deactivate" && parseInt(record.status) === 0)
-            return null;
-        if(action.descripcion === "deactivate" && parseInt(record.status) === 1)
-            return <i 
-                key={ action.id } 
-                title={ action.label }
-                className={`fa fa-pause fa-fw`}
-                style={{"fontSize": '14px'}}
-                onClick={() => handleAction(String(action.callMethod), record) } >
-
-                </i>
-        if(action.descripcion === "activate" && parseInt(record.status) === 0)
-            return <i 
-                key={ action.id } 
-                title={ action.label }
-                className={`fa fa-play fa-fw`}
-                style={{"fontSize": '14px'}}
-                onClick={() => handleAction(String(action.callMethod), record) } >
-
-                </i>
-        return null;
-    }
-
-    const renderColumn = (record: any, column: any) => {
-        const columnName: string = column.inputName;
-        if(columnName.includes(".")){
-            const deepColumns = columnName.split(".");
-            var deepRecord = record;
-            deepColumns.forEach(deepColumn => {
-                if(typeof(deepRecord[deepColumn]) === 'object' && String(deepRecord[deepColumn]) !== "null"){
-                    deepRecord = deepRecord[deepColumn]
-                }
-            });
-            return deepRecord[column.inputId];
-        }
-        if(typeof(record[columnName]) === 'object' && String(record[columnName]) !== "null"){
-            return record[columnName][column.inputId];
-        }
-        return record[columnName];
-    }
-
-    const handleAction = (action: string, record: any) => {
-        if(action === "changeStatus")
-            changeStatus(record.id, record.status);
-        if(action === "eliminar")
-            eliminar(record.id);
-        if(action === "update")
-            update(record.id);
-    }
-
-    const update = (recordId: number) => {
-        setRecordId(recordId);
-        setShowModal(true);
-    }
-    
-    const eliminar = (recordId: number) => {
-        deleteRecord(props.seccionMenu, recordId).then(response => {
-            if(!response.ok){
-                console.log("Error al eliminar registro");
-                console.log(response);
-                return;
-            }
-            setTable();
-        }).catch(error => console.error(error));
-    }
-
-    const changeStatus = (recordId: number, recordStatus: number) => {
-        getById(props.seccionMenu, recordId).then(response => {
-            if(!response.ok){
-                console.log("Error al obtener registro");
-                console.log(response);
-                return;
-            }
-            response.json().then(data => {
-                data.status = flipStatus(recordStatus);
-                updateRecord(props.seccionMenu, recordId, data).then(response => {
-                    if(!response.ok){
-                        console.log("Error al modificar registro");
-                        console.log(response);
-                        return;
-                    }
-                    response.json().then(data => {
-                        setTable();
-                    })
-                }).catch(error => console.error(error));
-            })
-        }).catch(error => console.error(error));
-    }
-
-    const { onDownload } = useDownloadExcel({
-        currentTableRef: tableRef.current,
-        filename: props.seccionMenu,
-        sheet: props.seccionMenu
-    })
-
     return (
         <>
             <Filters
                 seccionMenuId={ props.seccionMenuId }
                 seccionMenu={ props.seccionMenu }
-                setDataTable={ setData } />
-            <div className="table-container">
-                <table id="myTable" 
-                    className="table is-bordered is-striped is-hoverable is-fullwidth is-narrow" 
-                    ref={tableRef}>
-                    <thead>
-                        <tr>
-                            <th className="no-print">Acciones</th>
-                            {columns.map((column: any) => {
-                                return(
-                                    <th key={ column.id }> { column.inputLabel } </th>
-                                );
-                            })}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((record: any) => {
-                            return(
-                                <tr key={ record.id }>
-                                    <td>
-                                        {tableActions.map((action: Accion) => {
-                                            return(
-                                                renderAction(action, record)
-                                            );
-                                        })}
-                                    </td>
-                                    {columns.map((column: any) => {
-                                        return(
-                                            <td 
-                                                key={ column.id } 
-                                                style={ parseInt(record.status) === 0 ? recordInactive : {} }> 
-                                                { 
-                                                    renderColumn(record, column)
-                                                } 
-                                            </td>
-                                        );
-                                    })}
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table> 
-            </div>
+                inputsFilters={ inputsFilters }
+                setDataTable={ setDataTable } />
+            <Table
+                seccionMenuId={ props.seccionMenuId }
+                seccionMenu={ props.seccionMenu } 
+                columns={ columns } 
+                dataTable={ dataTable }
+                tableActions={ tableActions }
+                setTable={ setTable }
+                xls={ xls } />
             {/*<Pagination 
                 currentPage= { currentPage }
                 totalRows={ totalRows }
@@ -244,21 +104,6 @@ const Lista = (props: any) => {
                 setTableToPaginate={ setTableToPaginate }
                 dataTable={ data }
                 setDataTable={ setData } />*/}
-            {
-                Boolean(xls)
-                ? <button onClick={ onDownload } className="button" >Descargar excel</button>
-                : null
-            }
-            { 
-                Boolean(showModal) 
-                ? <ModalUpdate 
-                    seccionMenuId={ props.seccionMenuId } 
-                    seccionMenu={ props.seccionMenu }
-                    recordId={ recordId }
-                    stateShowModal={ setShowModal }
-                    setTable={ setTable } /> 
-                : null 
-            }
         </>
     );
 }
---
+++ src/app/ui/dashboard/Table.tsx
@@ -0,0 +1,197 @@
+import { useState, useRef } from "react";
+import ModalUpdate from '@/app/ui/dashboard/ModalUpdate';
+import {
+    getById, 
+    updateRecord,
+    deleteRecord 
+} from '../../api';
+import { flipStatus, castNullToString } from '../../funciones';
+import { Accion } from '../../entities';
+import { useDownloadExcel } from "react-export-table-to-excel";
+
+const Table = (props: any) => {
+    const tableRef = useRef(null);
+
+    const [showModal, setShowModal] = useState(false);
+    const [recordId, setRecordId] = useState(0);
+
+    const columns = props.columns;
+    const dataTable = props.dataTable;
+    const tableActions = props.tableActions;
+    const xls = props.xls;
+
+    const recordInactive = {
+        backgroundColor: "#ffe4f3"
+    };
+
+    const update = (recordId: number) => {
+        setRecordId(recordId);
+        setShowModal(true);
+    }
+    
+    const eliminar = (recordId: number) => {
+        deleteRecord(props.seccionMenu, recordId).then(response => {
+            if(!response.ok){
+                console.log("Error al eliminar registro");
+                console.log(response);
+                return;
+            }
+            props.setTable();
+        }).catch(error => console.error(error));
+    }
+
+    const changeStatus = (recordId: number, recordStatus: number) => {
+        getById(props.seccionMenu, recordId).then(response => {
+            if(!response.ok){
+                console.log("Error al obtener registro");
+                console.log(response);
+                return;
+            }
+            response.json().then(data => {
+                data.status = flipStatus(recordStatus);
+                updateRecord(props.seccionMenu, recordId, data).then(response => {
+                    if(!response.ok){
+                        console.log("Error al modificar registro");
+                        console.log(response);
+                        return;
+                    }
+                    response.json().then(data => {
+                        props.setTable();
+                    })
+                }).catch(error => console.error(error));
+            })
+        }).catch(error => console.error(error));
+    }
+
+    const handleAction = (action: string, record: any) => {
+        if(action === "changeStatus")
+            changeStatus(record.id, record.status);
+        if(action === "eliminar")
+            eliminar(record.id);
+        if(action === "update")
+            update(record.id);
+    }
+
+    const renderAction = (action: Accion, record: any) => {
+        if(action.callMethod === "xls")
+            return null;
+        if(action.callMethod !== "changeStatus")
+            return <i 
+                key={ action.id } 
+                title={ action.label }
+                className={`fa fa-${action.icon} fa-fw`}
+                style={{"fontSize": '14px'}}
+                onClick={() => handleAction(String(action.callMethod), record) } >
+
+                </i>
+        if(action.descripcion === "deactivate" && parseInt(record.status) === 0)
+            return null;
+        if(action.descripcion === "deactivate" && parseInt(record.status) === 1)
+            return <i 
+                key={ action.id } 
+                title={ action.label }
+                className={`fa fa-pause fa-fw`}
+                style={{"fontSize": '14px'}}
+                onClick={() => handleAction(String(action.callMethod), record) } >
+
+                </i>
+        if(action.descripcion === "activate" && parseInt(record.status) === 0)
+            return <i 
+                key={ action.id } 
+                title={ action.label }
+                className={`fa fa-play fa-fw`}
+                style={{"fontSize": '14px'}}
+                onClick={() => handleAction(String(action.callMethod), record) } >
+
+                </i>
+        return null;
+    }
+
+    const renderColumn = (record: any, column: any) => {
+        const columnName: string = column.inputName;
+        if(columnName.includes(".")){
+            const deepColumns = columnName.split(".");
+            var deepRecord = record;
+            deepColumns.forEach(deepColumn => {
+                if(typeof(deepRecord[deepColumn]) === 'object' && String(deepRecord[deepColumn]) !== "null"){
+                    deepRecord = deepRecord[deepColumn]
+                }
+            });
+            return deepRecord[column.inputId];
+        }
+        if(typeof(record[columnName]) === 'object' && String(record[columnName]) !== "null"){
+            return record[columnName][column.inputId];
+        }
+        return record[columnName];
+    }
+
+    const { onDownload } = useDownloadExcel({
+        currentTableRef: tableRef.current,
+        filename: props.seccionMenu,
+        sheet: props.seccionMenu
+    })
+
+    return (
+        <>
+            <div className="table-container">
+                <table id="myTable" 
+                    className="table is-bordered is-striped is-hoverable is-fullwidth is-narrow" 
+                    ref={ tableRef }>
+                    <thead>
+                        <tr>
+                            <th className="no-print">Acciones</th>
+                            {columns.map((column: any) => {
+                                return(
+                                    <th key={ column.id }> { column.inputLabel } </th>
+                                );
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {dataTable.map((record: any) => {
+                            return(
+                                <tr key={ record.id }>
+                                    <td>
+                                        {tableActions.map((action: Accion) => {
+                                            return(
+                                                renderAction(action, record)
+                                            );
+                                        })}
+                                    </td>
+                                    {columns.map((column: any) => {
+                                        return(
+                                            <td 
+                                                key={ column.id } 
+                                                style={ parseInt(record.status) === 0 ? recordInactive : {} }> 
+                                                { 
+                                                    renderColumn(record, column)
+                                                } 
+                                            </td>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            {
+                Boolean(xls)
+                ? <button onClick={ onDownload } className="button" >Descargar excel</button>
+                : null
+            }
+            { 
+                Boolean(showModal) 
+                ? <ModalUpdate 
+                    seccionMenuId={ props.seccionMenuId } 
+                    seccionMenu={ props.seccionMenu }
+                    recordId={ recordId }
+                    stateShowModal={ setShowModal }
+                    setTable={ props.setTable } /> 
+                : null 
+            }
+        </>
+    );
+}
+
+export default Table;
