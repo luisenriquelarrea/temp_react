59256d9a0092635ccaa67153ec95baeb7a87dcc5
luisenriquelarrea <enrique.larrea@alumno.udg.mx>
2024-07-31 17:14:53 -0600

    RFX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
         <head>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
         </head>
