74fb4c9e62a2728662aed661406e32dc70ff2079
luisenriquelarrea <enrique.larrea@alumno.udg.mx>
2025-07-16 13:36:14 -0600

    RFX

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,12 @@ const nextConfig = {
         apiKey: process.env.REACT_APP_API_KEY,
         apiToken: process.env.REACT_APP_API_TOKEN
     },
-    output: 'export',
     images: {
         unoptimized: true,
     },
     distDir: "build",
     trailingSlash: true,
-    assetPrefix: process.env.IS_PRODUCTION === 1 ? process.env.URL_PRODUCTION : undefined,
+    assetPrefix: (process.env.IS_PRODUCTION === 'true') ? process.env.URL_PRODUCTION : undefined,
     staticPageGenerationTimeout: 1000,
     reactStrictMode: false
 };
